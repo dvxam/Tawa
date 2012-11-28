742228d819bc5e81cdad281d46b5ce7f930f1e2c
Dvxam <dvxam@laposte.net>
2012-11-28 09:58:21 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tawa::Application.config.secret_token = '504fdeef1082c38fbfaf6d7601734326b545e842bdf16f7cc572231637b343207aba3adb2d14f8b256d6392c68e082d331522a9e93654fa49fcbb93b9bbe2636'
